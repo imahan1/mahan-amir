f37c3df99dbd3254860003bf0f4c585a26786451
amir <amir0011tataloo11228@gmail.com>
2025-09-22 20:00:55 +0330

    مین درست شد

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const spans = document.querySelectorAll("h1 span");
 spans.forEach((span, index) => {
   span.style.animationDelay = `${index * 0.1}s`;
 });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
